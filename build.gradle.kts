abdfc9d13aa767db642895a63fde9528a4ba38f0
Alex Katlein <dev@vemilyus.com>
2025-05-02 22:55:06 +0200

    chore: Upgraded dependencies and switched to new central maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,7 @@
-import name.remal.gradle_plugins.dsl.extensions.applyPlugin
-import name.remal.gradle_plugins.plugins.publish.ossrh.RepositoryHandlerOssrhExtension
-import org.jetbrains.dokka.DokkaConfiguration
-import org.jetbrains.dokka.Platform
-import org.jetbrains.dokka.gradle.DokkaTask
+import com.vanniktech.maven.publish.SonatypeHost
+import java.net.URI
 import java.util.*
-
-buildscript {
-    repositories { mavenCentral() }
-
-    dependencies {
-        classpath(libs.remalGradlePlugins)
-    }
-}
+import java.util.Calendar.YEAR
 
 plugins {
     kotlin("jvm")
@@ -20,9 +10,11 @@ plugins {
     jacoco
 
     alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
     alias(libs.plugins.license)
     alias(libs.plugins.release)
-    `maven-publish`
+
+    alias(libs.plugins.nmcp.maven.publish)
 }
 
 dependencies {
@@ -51,25 +43,6 @@ jacoco {
     toolVersion = "0.8.12"
 }
 
-tasks.withType<DokkaTask> {
-    dokkaSourceSets {
-        configureEach {
-            documentedVisibilities.set(setOf(DokkaConfiguration.Visibility.PUBLIC))
-            jdkVersion.set(21)
-            includes.from(project.files(), "packages.md")
-            platform.set(Platform.jvm)
-        }
-    }
-}
-
-val javadocJar by tasks.register<Jar>("javadocJar") {
-    group = JavaBasePlugin.DOCUMENTATION_GROUP
-    dependsOn(tasks.dokkaJavadoc)
-
-    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
-    archiveClassifier.set("javadoc")
-}
-
 license {
     exclude("**/*.exe")
 
@@ -77,7 +50,7 @@ license {
     skipExistingHeaders = true
 
     ext {
-        set("year", Calendar.getInstance().get(Calendar.YEAR))
+        set("year", Calendar.getInstance().get(YEAR))
     }
 }
 
@@ -85,71 +58,70 @@ release {
     tagTemplate = "v\$version"
 }
 
-val sourcesJar by tasks.register<Jar>("sourcesJar") {
-    from(sourceSets.main.get().allSource)
-    archiveClassifier.set("sources")
-}
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = "${project.group}"
-            artifactId = project.name
-            version = "${project.version}"
+dokka {
+    dokkaSourceSets.main {
+        val revision = "${project.version}".let { version ->
+            if (version.endsWith("-SNAPSHOT"))
+                "main"
+            else
+                "v$version"
+        }
 
-            from(components["java"])
+        sourceLink {
+            localDirectory.set(file("src/main/kotlin"))
 
-            artifact(sourcesJar)
-            artifact(javadocJar)
+            remoteUrl = URI("https://github.com/v47-io/nuprocess-shim/blob/$revision/src/main/kotlin")
+            remoteLineSuffix = "#L"
+        }
 
-            pom {
-                name.set("NuProcess Shim")
-                description.set("Provides the nuprocess API on top of the Java Process API")
-                url.set("https://github.com/v47-io/nuprocess-shim")
+        sourceLink {
+            localDirectory.set(file("src/main/java"))
 
-                licenses {
-                    license {
-                        name.set("BSD 3-Clause Clear License")
-                        url.set("https://spdx.org/licenses/BSD-3-Clause-Clear.html")
-                    }
-                }
+            remoteUrl = URI("https://github.com/v47-io/nuprocess-shim/blob/$revision/src/main/java")
+            remoteLineSuffix = "#L"
+        }
+    }
 
-                developers {
-                    developer {
-                        id.set("vemilyus")
-                        name.set("Alex Katlein")
-                        email.set("dev@vemilyus.com")
-                    }
-                }
+    pluginsConfiguration {
+        val copyright = "Copyright (c) ${Calendar.getInstance().get(YEAR)} Alex Katlein"
 
-                scm {
-                    connection.set("scm:git:git://github.com/v47-io/nuprocess-shim.git")
-                    developerConnection.set("scm:git:git://github.com/v47-io/nuprocess-shim.git")
-                    url.set("https://github.com/v47-io/nuprocess-shim")
-                }
-            }
+        html {
+            footerMessage = copyright
         }
     }
 }
 
-val ossrhUser: String? = project.findProperty("ossrhUser") as? String ?: System.getenv("OSSRH_USER")
-val ossrhPass: String? = project.findProperty("osshrPass") as? String ?: System.getenv("OSSRH_PASS")
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 
-if (!ossrhUser.isNullOrBlank() && !ossrhPass.isNullOrBlank() && !"${project.version}".endsWith("-SNAPSHOT")) {
-    applyPlugin("signing")
-    applyPlugin("name.remal.maven-publish-ossrh")
+    coordinates("${project.group}", project.name, "${project.version}")
 
-    publishing {
-        repositories {
-            @Suppress("DEPRECATION")
-            withConvention(RepositoryHandlerOssrhExtension::class) {
-                ossrh {
-                    credentials {
-                        username = ossrhUser
-                        password = ossrhPass
-                    }
-                }
+    pom {
+        name.set("NuProcess Shim")
+        description.set("Provides the nuprocess API on top of the Java Process API")
+        url.set("https://github.com/v47-io/nuprocess-shim")
+
+        licenses {
+            license {
+                name.set("BSD 3-Clause Clear License")
+                url.set("https://spdx.org/licenses/BSD-3-Clause-Clear.html")
             }
+        }
+
+        developers {
+            developer {
+                id.set("vemilyus")
+                name.set("Alex Katlein")
+                email.set("dev@vemilyus.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/v47-io/nuprocess-shim.git")
+            developerConnection.set("scm:git:git://github.com/v47-io/nuprocess-shim.git")
+            url.set("https://github.com/v47-io/nuprocess-shim")
         }
     }
 }
